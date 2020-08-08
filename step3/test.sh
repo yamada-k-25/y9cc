a3c498e4f29eb00a5cadd538e4d713da4b5313dc
Yamada.K <>
2020-08-08 16:37:24 +0900

    add step3

---
+++ step3/test.sh
@@ -0,0 +1,23 @@
+#1 /bin/bash
+assert() { 
+    expected="$1"
+    input="$2"
+
+    ./y9cc "$input" > tmp.s
+    cc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but go $actual"
+        exit 1
+    fi
+}
+
+assert 0 0
+assert 42 42
+assert 21 "5+20-4"
+
+echo OK
